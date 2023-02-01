b21eb29cc1cf80345c606b6d4da8569427ca57c0
Niek Weevers <n.weevers@am-impact.nl>
2023-02-01 16:05:53 +0100

    remove button accessible
    PORTAAL3-5003

--- src/components/Control.vue
+++ src/components/Control.vue
@@ -1,153 +1,167 @@
 <script>
-  import { onLeftClick, isPromise } from '../utils'
-  import SingleValue from './SingleValue'
-  import MultiValue from './MultiValue'
-  import DeleteIcon from './icons/Delete'
-  import ArrowIcon from './icons/Arrow'
+import { onLeftClick, isPromise } from "../utils";
+import SingleValue from "./SingleValue";
+import MultiValue from "./MultiValue";
+import DeleteIcon from "./icons/Delete";
+import ArrowIcon from "./icons/Arrow";
 
-  export default {
-    name: 'vue-treeselect--control',
-    inject: [ 'instance' ],
+export default {
+    name: "vue-treeselect--control",
+    inject: ["instance"],
 
     computed: {
-      /* eslint-disable valid-jsdoc */
-      /**
-       * Should show the "×" button that resets value?
-       * @return {boolean}
-       */
-      shouldShowX() {
-        const { instance } = this
+        /* eslint-disable valid-jsdoc */
+        /**
+         * Should show the "×" button that resets value?
+         * @return {boolean}
+         */
+        shouldShowX() {
+            const { instance } = this;
 
-        return (
-          instance.clearable &&
-          !instance.disabled &&
-          instance.hasValue &&
-          (this.hasUndisabledValue || instance.allowClearingDisabled)
-        )
-      },
+            return (
+                instance.clearable &&
+                !instance.disabled &&
+                instance.hasValue &&
+                (this.hasUndisabledValue || instance.allowClearingDisabled)
+            );
+        },
 
-      /**
-       * Should show the arrow button that toggles menu?
-       * @return {boolean}
-       */
-      shouldShowArrow() {
-        const { instance } = this
+        /**
+         * Should show the arrow button that toggles menu?
+         * @return {boolean}
+         */
+        shouldShowArrow() {
+            const { instance } = this;
 
-        if (!instance.alwaysOpen) return true
-        // Even with `alwaysOpen: true`, sometimes the menu is still closed,
-        // e.g. when the control is disabled.
-        return !instance.menu.isOpen
-      },
+            if (!instance.alwaysOpen) return true;
+            // Even with `alwaysOpen: true`, sometimes the menu is still closed,
+            // e.g. when the control is disabled.
+            return !instance.menu.isOpen;
+        },
 
-      /**
-       * Has any undisabled option been selected?
-       * @type {boolean}
-       */
-      hasUndisabledValue() {
-        const { instance } = this
+        /**
+         * Has any undisabled option been selected?
+         * @type {boolean}
+         */
+        hasUndisabledValue() {
+            const { instance } = this;
 
-        return (
-          instance.hasValue &&
-          instance.internalValue.some(id => !instance.getNode(id).isDisabled)
-        )
-      },
-      /* eslint-enable valid-jsdoc */
+            return (
+                instance.hasValue &&
+                instance.internalValue.some(
+                    (id) => !instance.getNode(id).isDisabled
+                )
+            );
+        },
+        /* eslint-enable valid-jsdoc */
     },
 
     methods: {
-      renderX() {
-        const { instance } = this
-        const title = instance.multiple ? instance.clearAllText : instance.clearValueText
+        renderX() {
+            const { instance } = this;
+            const title = instance.multiple
+                ? instance.clearAllText
+                : instance.clearValueText;
 
-        if (!this.shouldShowX) return null
+            if (!this.shouldShowX) return null;
 
-        return (
-          <div class="vue-treeselect__x-container" title={title} onMousedown={this.handleMouseDownOnX}>
-            <DeleteIcon class="vue-treeselect__x" />
-          </div>
-        )
-      },
+            return (
+                <div
+                    class="vue-treeselect__x-container"
+                    title={title}
+                    onMousedown={this.handleMouseDownOnX}
+                >
+                    <DeleteIcon class="vue-treeselect__x" />
+                </div>
+            );
+        },
 
-      renderArrow() {
-        const { instance } = this
-        const arrowClass = {
-          'vue-treeselect__control-arrow': true,
-          'vue-treeselect__control-arrow--rotated': instance.menu.isOpen,
-        }
+        renderArrow() {
+            const { instance } = this;
+            const arrowClass = {
+                "vue-treeselect__control-arrow": true,
+                "vue-treeselect__control-arrow--rotated": instance.menu.isOpen,
+            };
 
-        if (!this.shouldShowArrow) return null
+            if (!this.shouldShowArrow) return null;
 
-        return (
-          <div class="vue-treeselect__control-arrow-container" onMousedown={this.handleMouseDownOnArrow}>
-            <ArrowIcon class={arrowClass} />
-          </div>
-        )
-      },
+            return (
+                <div
+                    class="vue-treeselect__control-arrow-container"
+                    onMousedown={this.handleMouseDownOnArrow}
+                >
+                    <ArrowIcon class={arrowClass} />
+                </div>
+            );
+        },
 
-      handleMouseDownOnX: onLeftClick(function handleMouseDownOnX(evt) {
-        /**
-         * We don't use async/await here because we don't want
-         * to rely on Babel polyfill or regenerator runtime.
-         * See: https://babeljs.io/docs/plugins/transform-regenerator/
-         * We also don't want to assume there is a global `Promise`
-         * class, since we are targeting to support IE9 without the
-         * need of any polyfill.
-         */
+        handleMouseDownOnX: onLeftClick(function handleMouseDownOnX(evt) {
+            /**
+             * We don't use async/await here because we don't want
+             * to rely on Babel polyfill or regenerator runtime.
+             * See: https://babeljs.io/docs/plugins/transform-regenerator/
+             * We also don't want to assume there is a global `Promise`
+             * class, since we are targeting to support IE9 without the
+             * need of any polyfill.
+             */
 
-        evt.stopPropagation()
-        evt.preventDefault()
+            evt.stopPropagation();
+            evt.preventDefault();
 
-        const { instance } = this
-        const result = instance.beforeClearAll()
-        const handler = shouldClear => {
-          if (shouldClear) instance.clear()
-        }
+            const { instance } = this;
+            const result = instance.beforeClearAll();
+            const handler = (shouldClear) => {
+                if (shouldClear) instance.clear();
+            };
 
-        if (isPromise(result)) {
-          // The handler will be called async.
-          result.then(handler)
-        } else {
-          // Keep the same behavior here.
-          setTimeout(() => handler(result), 0)
-          // Also, note that IE9 requires:
-          //   setTimeout(() => fn(...args), delay)
-          // Instead of:
-          //   setTimeout(fn, delay, ...args)
-        }
-      }),
+            if (isPromise(result)) {
+                // The handler will be called async.
+                result.then(handler);
+            } else {
+                // Keep the same behavior here.
+                setTimeout(() => handler(result), 0);
+                // Also, note that IE9 requires:
+                //   setTimeout(() => fn(...args), delay)
+                // Instead of:
+                //   setTimeout(fn, delay, ...args)
+            }
+        }),
 
-      handleMouseDownOnArrow: onLeftClick(function handleMouseDownOnArrow(evt) {
-        evt.preventDefault()
-        evt.stopPropagation()
+        handleMouseDownOnArrow: onLeftClick(function handleMouseDownOnArrow(
+            evt
+        ) {
+            evt.preventDefault();
+            evt.stopPropagation();
 
-        const { instance } = this
+            const { instance } = this;
 
-        // Focus the input or prevent blurring.
-        instance.focusInput()
-        instance.toggleMenu()
-      }),
+            // Focus the input or prevent blurring.
+            instance.focusInput();
+            instance.toggleMenu();
+        }),
 
-      // This is meant to be called by child `<Value />` component.
-      renderValueContainer(children) {
-        return (
-          <div class="vue-treeselect__value-container">
-            {children}
-          </div>
-        )
-      },
+        // This is meant to be called by child `<Value />` component.
+        renderValueContainer(children) {
+            return (
+                <div class="vue-treeselect__value-container">{children}</div>
+            );
+        },
     },
 
     render() {
-      const { instance } = this
-      const ValueContainer = instance.single ? SingleValue : MultiValue
+        const { instance } = this;
+        const ValueContainer = instance.single ? SingleValue : MultiValue;
 
-      return (
-        <div class="vue-treeselect__control" onMousedown={instance.handleMouseDown}>
-          <ValueContainer ref="value-container" />
-          {this.renderX()}
-          {this.renderArrow()}
-        </div>
-      )
+        return (
+            <div
+                class="vue-treeselect__control"
+                onMousedown={instance.handleMouseDown}
+            >
+                <ValueContainer ref="value-container" />
+                {this.renderX()}
+                {this.renderArrow()}
+            </div>
+        );
     },
-  }
+};
 </script>
--- src/utils/onLeftClick.js
+++ src/utils/onLeftClick.js
@@ -1,7 +1,10 @@
 export function onLeftClick(mouseDownHandler) {
-  return function onMouseDown(evt, ...args) {
-    if (evt.type === 'mousedown' && evt.button === 0) {
-      mouseDownHandler.call(this, evt, ...args)
-    }
-  }
+    return function onMouseDown(evt, ...args) {
+        if (
+            (evt.type === "mousedown" || evt.type === "click") &&
+            evt.button === 0
+        ) {
+            mouseDownHandler.call(this, evt, ...args);
+        }
+    };
 }
